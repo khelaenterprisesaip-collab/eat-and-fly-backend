87481fe066b1747b85f4695e51fe9e7b0a095fca
Gurpreet Singh <103951591+Igurprxxt@users.noreply.github.com>
2025-11-26 22:18:29 +0530

    updated

--- routes/api/Invoice.route.js
+++ routes/api/Invoice.route.js
@@ -1,8 +1,10 @@
 const addInvoice = require("../../controllers/invoice/addInvoice");
+const getInvoices = require("../../controllers/invoice/getInvoice");
 
 const router = require("express").Router();
 
 // GET /api/products
 router.post("/", addInvoice); // Add auth middleware here
+router.get("/", getInvoices); // Add auth middleware here
 
 module.exports = router;
