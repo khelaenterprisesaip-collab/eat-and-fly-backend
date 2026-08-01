7df7bc7cb37bddf64c05965375c580f8693f0a0f
Gurpreet Singh <gurpreet@majhacapital.com>
2026-08-01 16:34:06 +0530

    updated code

--- controllers/invoice/getInvoice.js
+++ controllers/invoice/getInvoice.js
@@ -1,8 +1,10 @@
 const InvoiceModel = require("../../models/Invoice.model");
+const UserModel = require("../../models/User.model");
 
 const getInvoices = async (req, res) => {
   try {
     let { page = 1, limit = 10, search = "", airport = "" } = req.query;
+    const currentUser = req.user;
 
     page = Number(page);
     limit = Number(limit);
@@ -12,13 +14,30 @@ const getInvoices = async (req, res) => {
     if (search) {
       const regex = new RegExp(search, "i");
 
-      query.$or = [
-        { invoiceNumber: regex },
-        { "customer.name": regex },
-      ];
+      query.$or = [{ invoiceNumber: regex }, { "customer.name": regex }];
     }
 
-    if (airport) {
+    if (currentUser?.role === "staff") {
+      const existingUser = await UserModel.findOne({
+        uuid: currentUser?.uuid,
+      }).select("airport");
+
+      if (!existingUser) {
+        return res.status(404).json({
+          success: false,
+          message: "User not found",
+        });
+      }
+
+      if (!existingUser?.airport) {
+        return res.status(403).json({
+          success: false,
+          message: "Airport is not assigned to current user",
+        });
+      }
+
+      query.airport = "ludhiana";
+    } else if (airport) {
       query.airport = airport.toLowerCase();
     }
 
